feb73e81b499da72c10f552f60ff36e2eabd3d56
Sagar Bajpai <ashversache@gmail.com>
2020-06-20 05:11:59 +0530

    move images to src folder

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import "../../styles/header.css";
 
 // Data required for the function to render the DOM:
-import socialLogo from "../../images/social-logo.png";
+import socialLogo from "../images/social-logo.png";
 
 /**
  * A function that creates & returns the header dom element
