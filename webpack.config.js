a0c2c012f089f1241db61e47b15ed45e38f9aff0
Sagar Bajpai <ashversache@gmail.com>
2020-07-11 21:06:55 +0530

    babel with all rules inside webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,27 @@ module.exports = () => ({
           },
         },
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+            plugins: [],
+          },
+        },
+      },
     ],
   },
 });
