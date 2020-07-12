c90c697995836f82cafeef28e4d967e2bccbba11
Sagar Bajpai <ashversache@gmail.com>
2020-07-12 23:20:04 +0530

    webpack dev server installed

--- configs/development.config.js
+++ configs/development.config.js
@@ -5,4 +5,9 @@ module.exports = () => ({
     filename: "bundle.js",
     path: path.resolve(__dirname, "../dist"),
   },
+  devServer: {
+    //https://webpack.js.org/configuration/dev-server/
+    contentBase: "../dist",
+    port: 3001,
+  },
 });
