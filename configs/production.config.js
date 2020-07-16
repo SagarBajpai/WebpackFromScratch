ef3ec88dea67c5f903be786a388f682dc7793360
Sagar Bajpai <ashversache@gmail.com>
2020-07-16 13:33:56 +0530

    OptimizeCssAssetsPlugin & terserPlugin plugin integrated

--- configs/production.config.js
+++ configs/production.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //https://webpack.js.org/plugins/mini-css-extract-plugin/
-
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin"); //https://webpack.js.org/plugins/mini-css-extract-plugin/
+const terserPlugin = require("terser-webpack-plugin"); //https://webpack.js.org/plugins/terser-webpack-plugin/
 module.exports = () => ({
   output: {
     //devtool: "inline-source-map",
     filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "../dist"),
+  },
+  optimization: {
+    minimizer: [new terserPlugin(), new OptimizeCssAssetsPlugin()],
   },
   module: {
     rules: [
