35d9c102505e51e4891a5cc7e368d0fe807b9d9c
Sagar Bajpai <ashversache@gmail.com>
2020-07-17 22:48:03 +0530

    compression enabled

--- configs/production.config.js
+++ configs/production.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //https://webpack.js.org/plugins/mini-css-extract-plugin/
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin"); //https://webpack.js.org/plugins/mini-css-extract-plugin/
 const TerserPlugin = require("terser-webpack-plugin"); //https://webpack.js.org/plugins/terser-webpack-plugin/
+const CompressWebpackPlugin = require("compression-webpack-plugin");
+
 module.exports = () => ({
   devtool: "source-map",
   output: {
