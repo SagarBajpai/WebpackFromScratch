31d4e2f166104bf2bff0cb8208ffedc098743e0d
Sagar Bajpai <ashversache@gmail.com>
2024-01-17 17:10:06 +0530

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
-module.exports = () => ({
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-});
+module.exports = (props) => {
+  return {
+    mode: props.mode,
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  };
+};
